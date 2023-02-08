2c037fcbc05ff97b71a2925a351c89f9e7642da2
imranBappy <imranbappy.official@gmail.com>
2023-02-08 16:50:52 +0600

    basic express server setuo

--- client/src/components/Bodypart.js
+++ client/src/components/Bodypart.js
@@ -1,79 +1,76 @@
 import React, { useState } from "react";
 import BodyImage from "./bodyImage";
 
-const Bodypart = ()=>{
+const Bodypart = () => {
     // const lst =[1,2,3,4,5,6];
     // const [state,setState] = useState(lst);
-    const [imgState,setImgState] = useState(BodyImage);
-    
-    return(
-       
+    const [imgState, setImgState] = useState(BodyImage);
+    return (
         <>
-        <section className="mt-5">
-            <div className="relative container m-auto">
-                <img src="./images/bodyFirstImg1.png" className="relative w-full" alt="" srcset="" />
-                <h3 className="absolute top-52 left-60  text-white text-6xl">Вручение сертификатов<br/> выпускникам курса 2020 г.</h3>
-            </div>
-        </section>
-        
-        <section>
-            <div className="container m-auto flex mt-20 mb-20">
-                <div className="w-1/2">
-                    <img src="./images/secondbodyimg2.png" className="w-11/12" alt="" srcset="" />
+            <section className="mt-5">
+                <div className="relative container m-auto">
+                    <img src="./images/bodyFirstImg1.png" className="relative w-full" alt="" srcset="" />
+                    <h3 className="absolute top-52 left-60  text-white text-6xl">Вручение сертификатов<br /> выпускникам курса 2020 г.</h3>
                 </div>
-                <div className="w-1/2 mt-5">
-                    <h2 className="text-4xl">Вручение сертификатов <br/> выпускникам курса</h2>
-                    <p className="text-lg mt-4 mb-20 ">
-                    Сегодня 3 декабря 2020 года в Академии цифровых инноваций состоялось вручение сертификатов выпускникам курса Фронтенд-разработки.
+            </section>
+            <section>
+                <div className="container m-auto flex mt-20 mb-20">
+                    <div className="w-1/2">
+                        <img src="./images/secondbodyimg2.png" className="w-11/12" alt="" srcset="" />
+                    </div>
+                    <div className="w-1/2 mt-5">
+                        <h2 className="text-4xl">Вручение сертификатов <br /> выпускникам курса</h2>
+                        <p className="text-lg mt-4 mb-20">
+                            Сегодня 3 декабря 2020 года в Академии цифровых инноваций состоялось вручение сертификатов выпускникам курса Фронтенд-разработки.
                         </p>
                         <a className="mr-12" href="#"><i className="fa-solid fa-arrow-left p-2"></i>Предыдущий</a>
                         <a href="#">Следующий<i className="fa-solid fa-arrow-right p-2"></i></a>
+                    </div>
                 </div>
-            </div>
-        </section>
-       
-        <section className="container m-auto flex flex-wrap ">
-        {
-            imgState.map((curEle)=>{
-            return(
-                <>
-                <div className="w-1/3 p-6 ">
-                <img src={curEle.image} className="w-full" alt="" srcset="" />
-                <div>
-                    <h1 className="text-lg mt-3 mb-3">Вручение сертификатов<br/> выпускникам курса</h1>
-                    <p className="flex justify-center mb-9">Сегодня 3 декабря 2020 года в Академии цифровых инноваций состоялось вручение сертификатов</p>
-                </div>
-            </div>
-                </>
-            );
-        })
-        }
-        </section>
+            </section>
+
+            <section className="container m-auto flex flex-wrap ">
+                {
+                    imgState.map((curEle) => {
+                        return (
+                            <>
+                                <div className="w-1/3 p-6 ">
+                                    <img src={curEle.image} className="w-full" alt="" srcset="" />
+                                    <div>
+                                        <h1 className="text-lg mt-3 mb-3">Вручение сертификатов<br /> выпускникам курса</h1>
+                                        <p className="flex justify-center mb-9">Сегодня 3 декабря 2020 года в Академии цифровых инноваций состоялось вручение сертификатов</p>
+                                    </div>
+                                </div>
+                            </>
+                        );
+                    })
+                }
+            </section>
 
 
-        <section >
-            <div className="container flex flex-row m-auto" >
-                
-                <div>
-                <img src="./images/fourthbody1.png" className="" alt="" srcset="" />
-                </div>
-                <div>
-                    <img src="./images/fourthbody2.png" className="w-full" alt="" srcset="" />
-                </div>
-            </div>
+            <section >
+                <div className="container flex flex-row m-auto" >
 
-            <div className="container m-auto flex">
-             <div>
-                    <img src="./images/fourthbody3.png" className="" alt="" srcset="" />
+                    <div>
+                        <img src="./images/fourthbody1.png" className="" alt="" srcset="" />
+                    </div>
+                    <div>
+                        <img src="./images/fourthbody2.png" className="w-full" alt="" srcset="" />
+                    </div>
                 </div>
-                <div>
-                    <img src="./images/fourthbody4.png" className="" alt="" srcset="" />
+
+                <div className="container m-auto flex">
+                    <div>
+                        <img src="./images/fourthbody3.png" className="" alt="" srcset="" />
+                    </div>
+                    <div>
+                        <img src="./images/fourthbody4.png" className="" alt="" srcset="" />
+                    </div>
+                    <div>
+                        <img src="./images/fourthbody5.png" className="" alt="" srcset="" />
+                    </div>
                 </div>
-                <div>
-                    <img src="./images/fourthbody5.png" className="" alt="" srcset="" />
-                </div>
-                </div>
-                </section>
+            </section>
 
 
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,29 +3,25 @@ import React from 'react';
 const Nav = () => {
     return (
         <div>
-            
             <nav>
-
-<div className="flex justify-between  h-16 ">
-  
-  <div className='ml-7 h-16 w-16 '>
-    {/* <h1 className="ml-20 font-bold font-serif text-3xl">Cripto</h1> */}
-    <img src="./images/maxresdefault 1.png" alt="" srcset="" />
-  </div>
-  <div className='mt-4 inline'>
-    <ul className="flex space-x-6 text-lg" >
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Education</a></li>
-      <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
-      <div><li className="mr-10 ml-10 text-xl"><a href="#" >login</a></li></div>
-    </ul>
-  </div>
-</div>
-</nav>
-
-   </div>
+                <div className="flex justify-between  h-16 ">
+                    <div className='ml-7 h-16 w-16 '>
+                        {/* <h1 className="ml-20 font-bold font-serif text-3xl">Cripto</h1> */}
+                        <img src="./images/maxresdefault 1.png" alt="" srcset="" />
+                    </div>
+                    <div className='mt-4 inline'>
+                        <ul className="flex space-x-6 text-lg" >
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Education</a></li>
+                            <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+                            <div><li className="mr-10 ml-10 text-xl"><a href="#" >login</a></li></div>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     );
 };
 
