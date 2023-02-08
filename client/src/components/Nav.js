7a2576165ccb53b9d9c4e17ccc3b221b4d660a07
imranBappy <imranbappy.official@gmail.com>
2023-02-08 10:42:11 +0600

    create-Home-page

---
+++ client/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Nav;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import Nav from "../components/Nav"
+
+const Home = () => {
+
+    return <>
+        <header >
+            <Nav />
+        </header>
+        <main>
+            body
+        </main>
+    </>
+}
+export default Home;
