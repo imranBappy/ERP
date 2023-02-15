901217287e007287d8fe499a33a2867398ec99ba
Masum-SM <unusmasum0@gmail.com>
2023-02-16 01:30:16 +0600

    update navbar and banner

--- client/src/components/Shared/Navbar/Navbar.js
+++ client/src/components/Shared/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 max-w-screen-xl mx-auto">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
