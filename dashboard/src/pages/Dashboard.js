380ac2d34230485729d832a6adf76aefa86feb8a
imranBappy <imranbappy.official@gmail.com>
2023-02-16 18:59:14 +0600

    info section done

--- dashboard/src/pages/Dashboard.js
+++ dashboard/src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/dashboard.css';
 import DashLink from '../components/DashLink';
-
+import student from '../imgs/student.png'
 import { AiOutlineClose } from 'react-icons/ai';
 import { GoThreeBars } from 'react-icons/go';
 
@@ -20,13 +20,12 @@ const Dashboard = () => {
                         <AiOutlineClose className=' text-3xl text-slate-50 ' />
                     </button>
                 </div>
-
                 <div className="sidebar__logo flex justify-center mt-5">
                     <img className='w-20' src={logo} alt="logo" />
                 </div>
                 <div className="sidebar__menu ">
                     <ul>
-                        <li className=' text-gray-400 ml-5 text-sm font-mono'> Main Menu </li>
+                        <li className=' text-gray-400 ml-5 text-sm  font-mono'> Main Menu </li>
                         <DashLink text="Dashboard" />
                         <DashLink text="Dashboard" />
                         <DashLink text="Dashboard" />
@@ -36,7 +35,7 @@ const Dashboard = () => {
                 </div>
             </div>
             <div className="dashboard__wrapper grow  ">
-                <div className="dashboard__header flex justify-between h-14 shadow-md mb-5 ">
+                <div className="dashboard__header flex justify-between h-14 shadow-md  ">
 
                     <div className="dashboard__header_lift flex gap-6">
                         <div className='close-btn'>
@@ -55,18 +54,52 @@ const Dashboard = () => {
                         </div>
                     </div>
                 </div>
-                <div className="dashboard__content  bg-slate-100 ">
-                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, odit!</h1>
+                <div className="dashboard__content  bg-gray-100 p-5 ">
+                    <h2 className=' text-2xl font-semibold'> Welcome Admin!</h2>
 
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque pariatur nihil nam voluptates corrupti magnam, facilis magni voluptatum. Officiis, eum.</p>
+                    <div className="dashboard__card_wrapper flex flex-wrap mt-5 justify-between">
+                        <div className="info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
+                            <div className="card__left">
+                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
+                                <p className=' font-bold text-2xl'>50000</p>
+                            </div>
+                            <div className="card__right">
+                                <img width="50" src={student} alt="" />
+                            </div>
+                        </div>
 
+                        <div className="info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
+                            <div className="card__left">
+                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
+                                <p className=' font-bold text-2xl'>50000</p>
+                            </div>
+                            <div className="card__right">
+                                <img width="50" src={student} alt="" />
+                            </div>
+                        </div>
+                        <div className="info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
+                            <div className="card__left">
+                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
+                                <p className=' font-bold text-2xl'>50000</p>
+                            </div>
+                            <div className="card__right">
+                                <img width="50" src={student} alt="" />
+                            </div>
+                        </div>
 
-                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, odit!</h1>
-
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque pariatur nihil nam voluptates corrupti magnam, facilis magni voluptatum. Officiis, eum.</p>
+                        <div className="info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
+                            <div className="card__left">
+                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
+                                <p className=' font-bold text-2xl'>50000</p>
+                            </div>
+                            <div className="card__right">
+                                <img width="50" src={student} alt="" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
