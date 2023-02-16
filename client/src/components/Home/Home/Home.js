aae618e568a3fdbfd7c37affacdc17a9abe38ed7
Masum-SM <unusmasum0@gmail.com>
2023-02-16 11:10:58 +0600

    welcome section

--- client/src/components/Home/Home/Home.js
+++ client/src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import WelcomeSection from '../WelcomSection/WelcomeSection';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <WelcomeSection></WelcomeSection>
         </div>
     );
 };
