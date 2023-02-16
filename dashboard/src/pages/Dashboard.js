80651b04ca34ba0f6627b6ec77dabac0564d80e5
Mehedi <mehedihassangd@gmail.com>
2023-02-16 23:51:02 +0600

    I have added avatar

--- dashboard/src/pages/Dashboard.js
+++ dashboard/src/pages/Dashboard.js
@@ -3,6 +3,9 @@ import logo from '../logo.svg';
 import '../styles/dashboard.css';
 import DashLink from '../components/DashLink';
 import student from '../imgs/student.png'
+import award from '../imgs/award.png'
+import Department from '../imgs/Department.png'
+import Revenue from '../imgs/Revenue.png'
 import { AiOutlineClose } from 'react-icons/ai';
 import { GoThreeBars } from 'react-icons/go';
 
@@ -27,10 +30,10 @@ const Dashboard = () => {
                     <ul>
                         <li className='my-3 text-gray-400 ml-5 text-sm  font-mono'> Main Menu </li>
                         <DashLink text="Dashboard" />
-                        <DashLink text="Dashboard" />
-                        <DashLink text="Dashboard" />
-                        <DashLink text="Dashboard" />
-                        <DashLink text="Dashboard" />
+                        <DashLink text="Students" />
+                        <DashLink text="Teachers" />
+                        <DashLink text="Department" />
+                        <DashLink text="Invoice" />
                     </ul>
                 </div>
             </div>
@@ -60,8 +63,8 @@ const Dashboard = () => {
                     <div className="dashboard__card_wrapper flex  t-5 gap-5  flex-wrap  justify-between">
                         <div className=" flex-grow info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
                             <div className="card__left">
-                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
-                                <p className=' font-bold text-2xl'>50000</p>
+                                <p className='text-gray-500 text-lg  font-semibold'>students</p>
+                                <p className=' font-bold text-2xl'>50055</p>
                             </div>
                             <div className="card__right">
                                 <img width="50" src={student} alt="" />
@@ -70,30 +73,30 @@ const Dashboard = () => {
 
                         <div className=" flex-grow info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
                             <div className="card__left">
-                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
-                                <p className=' font-bold text-2xl'>50000</p>
+                                <p className='text-gray-500 text-lg  font-semibold'>award</p>
+                                <p className=' font-bold text-2xl'>50+</p>
                             </div>
                             <div className="card__right">
-                                <img width="50" src={student} alt="" />
+                                <img width="50" src={award} alt="" />
                             </div>
                         </div>
                         <div className=" flex-grow info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
                             <div className="card__left">
-                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
-                                <p className=' font-bold text-2xl'>50000</p>
+                                <p className='text-gray-500 text-lg  font-semibold'>Department</p>
+                                <p className=' font-bold text-2xl'>30+</p>
                             </div>
                             <div className="card__right">
-                                <img width="50" src={student} alt="" />
+                                <img width="50" src={Department} alt="" />
                             </div>
                         </div>
 
                         <div className="flex-grow info_card flex bg-white  basis-80 justify-between p-5 rounded-md shawdow-md">
                             <div className="card__left">
-                                <p className='text-gray-500 text-lg  font-semibold'>Soudent</p>
-                                <p className=' font-bold text-2xl'>50000</p>
+                                <p className='text-gray-500 text-lg  font-semibold'>Revenue</p>
+                                <p className=' font-bold text-2xl'>$505</p>
                             </div>
                             <div className="card__right">
-                                <img width="50" src={student} alt="" />
+                                <img width="50" src={Revenue} alt="" />
                             </div>
                         </div>
                     </div>
