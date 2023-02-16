ca3c9783e106bf38c0b9555fc582e3eb8e9e68ac
Masum-SM <unusmasum0@gmail.com>
2023-02-16 16:09:24 +0600

    feature section

---
+++ client/src/components/Home/FeatureContent/FeatureCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FeatureCard = ({card}) => {
+    const {icon,num,des} = card;
+    return (
+        <div className='f_card_container'>
+            <div>{icon}</div>
+            <h2>{num}</h2>
+            <h3>{des}</h3>
+        </div>
+    );
+};
+
+export default FeatureCard;
--- client/src/components/Home/Home/Home.js
+++ client/src/components/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import FeatureContent from '../FeatureContent/FeatureContent';
 import RegisterNow from '../RegisterNow/RegisterNow';
 import Teachers from '../Teachers/Teachers';
 import WelcomeSection from '../WelcomSection/WelcomeSection';
@@ -11,6 +12,7 @@ const Home = () => {
             <WelcomeSection></WelcomeSection>
             <RegisterNow></RegisterNow>
             <Teachers></Teachers>
+            <FeatureContent></FeatureContent>
         </div>
     );
 };
