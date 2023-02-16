304aaa3c4e2fd1d7ae1b6e0b8b89cccb3d9c9e08
Masum-SM <unusmasum0@gmail.com>
2023-02-16 14:48:55 +0600

    teacher section

--- client/src/components/Home/Home/Home.js
+++ client/src/components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import RegisterNow from '../RegisterNow/RegisterNow';
+import Teachers from '../Teachers/Teachers';
 import WelcomeSection from '../WelcomSection/WelcomeSection';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <WelcomeSection></WelcomeSection>
             <RegisterNow></RegisterNow>
+            <Teachers></Teachers>
         </div>
     );
 };
--- client/src/components/Home/WelcomSection/WelcomeSection.js
+++ client/src/components/Home/WelcomSection/WelcomeSection.js
@@ -5,26 +5,26 @@ import "./WelcomeSection.css";
 import "./WelcomeSection.css";
 
 const WelcomeSection = () => {
-    const cardData=[
+    const cardData = [
         {
-            id:1,
+            id: 1,
             icon: <i class="fa-solid fa-graduation-cap"></i>,
-            name : "Program",
-            description : "The technical writing profession is continuously evolving as technical logitech...",
+            name: "Program",
+            description: "The technical writing profession is continuously evolving as technical logitech...",
 
         },
         {
-            id:2,
+            id: 2,
             icon: <i class="fa-solid fa-chart-pie"></i>,
-            name : "Affordability",
-            description : "The technical writing profession is continuously evolving as technical logitech...",
+            name: "Affordability",
+            description: "The technical writing profession is continuously evolving as technical logitech...",
 
         },
         {
-            id:3,
+            id: 3,
             icon: <i class="fa-sharp fa-solid fa-certificate"></i>,
-            name : "Certification",
-            description : "The technical writing profession is continuously evolving as technical logitech...",
+            name: "Certification",
+            description: "The technical writing profession is continuously evolving as technical logitech...",
 
         },
 
@@ -33,21 +33,21 @@ const WelcomeSection = () => {
     return (
         <div className='welcom_section mx-auto '>
             <div className="wel_container">
-            <div className='welcome_header'>
-                <h1 className='text-4xl'>Welcome to University</h1>
-                <h3>Study Point is a international leader in teaching students to write effectively,learn from each other and think for themselves.</h3>
-            </div>
-            
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 cards'>
-                {
-                    cardData.map(card => <SingleCard
-                    key={card.id}
-                    card = {card}
-                    ></SingleCard>)
-                }
+                <div className='welcome_header'>
+                    <h1 className='text-4xl'>Welcome to University</h1>
+                    <h3>Study Point is a international leader in teaching students to write effectively,learn from each other and think for themselves.</h3>
+                </div>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 cards'>
+                    {
+                        cardData.map(card => <SingleCard
+                            key={card.id}
+                            card={card}
+                        ></SingleCard>)
+                    }
 
 
-            </div>
+                </div>
             </div>
 
         </div>
