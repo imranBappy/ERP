7f6fb81dd3296345cc1b7305e1c12e164fe1d350
imranBappy <imranbappy.official@gmail.com>
2023-02-16 18:10:28 +0600

    header section done

--- dashboard/src/components/DashLink.js
+++ dashboard/src/components/DashLink.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { RxDashboard } from 'react-icons/rx';
+import { MdNavigateNext } from 'react-icons/md';
+import '../styles/DashLink.css'
 const DashLink = (props) => {
     return (
         <>
-            <li className=' h-10 flex justify-center items-center'> <RxDashboard/> <NavLink to='/'>{props.text}</NavLink> </li>
+            <li className='dash__link  h-10 flex justify-center items-center'>
+                <RxDashboard />
+                <span>
+                    <NavLink to='/'>{props.text}</NavLink>
+                </span>
+                <MdNavigateNext className='nav_next' />
+            </li>
         </>
     );
 };
--- dashboard/src/pages/Dashboard.js
+++ dashboard/src/pages/Dashboard.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/dashboard.css';
 import DashLink from '../components/DashLink';
+
+import { AiOutlineClose } from 'react-icons/ai';
+import { GoThreeBars } from 'react-icons/go';
+
+
 const Dashboard = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => {
@@ -10,15 +15,18 @@ const Dashboard = () => {
     return (
         <div className='dashboard__wrapper flex w-full relative'>
             <div className={`drawer ${open ? "drawer-open" : "drawer-close"}  `}>
-                <div className='close-btn'>
-                    <button onClick={handleOpen} >Close</button>
+                <div className='close-btn close-btn-ops '>
+                    <button onClick={handleOpen} className='ring-2 bg-sky-600 rounded-full ring-sky-600 shadow-lg' >
+                        <AiOutlineClose className=' text-3xl text-slate-50 ' />
+                    </button>
                 </div>
+
                 <div className="sidebar__logo flex justify-center mt-5">
                     <img className='w-20' src={logo} alt="logo" />
                 </div>
                 <div className="sidebar__menu ">
                     <ul>
-                        <li> Main Menu </li>
+                        <li className=' text-gray-400 ml-5 text-sm font-mono'> Main Menu </li>
                         <DashLink text="Dashboard" />
                         <DashLink text="Dashboard" />
                         <DashLink text="Dashboard" />
@@ -28,16 +36,23 @@ const Dashboard = () => {
                 </div>
             </div>
             <div className="dashboard__wrapper grow  ">
-                <div className="dashboard__header flex justify-between">
+                <div className="dashboard__header flex justify-between h-14 shadow-md mb-5 ">
 
                     <div className="dashboard__header_lift flex gap-6">
                         <div className='close-btn'>
-                            <button onClick={handleOpen}>OPEN</button>
+                            <button onClick={handleOpen} className='bg-sky-600 px-2 py-1 rounded-md m-2'>
+                                <GoThreeBars className='  text-3xl text-slate-50' />
+                            </button>
                         </div>
-                        <h1>Dashboard</h1>
                     </div>
-                    <div className="dashboard__header_right">
-                        <h1>Profile</h1>
+                    <div className="dashboard__header_right flex gap-2 mr-4 mt-2">
+                        <div>
+                            <img className=' w-10 rounded-full ring-1 ring-offset-2' src="https://preschool.dreamguystech.com/template/assets/img/profiles/avatar-01.jpg" alt="profile-ric" />
+                        </div>
+                        <div>
+                            <h4 className=' font-medium text-lg leading-5'>Imran Hossen</h4>
+                            <p className=' font-semibold text-sm	text-gray-500'>Admin</p>
+                        </div>
                     </div>
                 </div>
                 <div className="dashboard__content  bg-slate-100 ">
