0dbd749c9f34b8d14d035d90cc5bba0714a91197
imranBappy <imranbappy.official@gmail.com>
2023-02-24 16:02:28 +0600

    add student form error fix

--- dashboard/src/pages/AddStudentInputfield.js
+++ dashboard/src/pages/AddStudentInputfield.js
@@ -1,64 +1,70 @@
-const input_field =[
+const input_field = [
     {
         label: "Full Name",
         placeHolder: "Full Name",
         type: "text",
         name: "name",
-    },{
+    },
+    {
+        label: "Email",
+        placeHolder: "Email",
+        type: "email",
+        name: "email",
+    },
+    {
+        label: "Phone",
+        placeHolder: "Student's Phone Number",
+        type: "Phone",
+        name: "phone",
+    },
+    {
         label: "Father Name",
         placeHolder: "Father Name",
         type: "text",
         name: "fatherName",
-    },{
+    }, {
         label: "Mother Name",
         placeHolder: "Mother Name",
         type: "text",
         name: "motherName",
-    },,{
+    },
+    {
         label: "Guardian Phone",
         placeHolder: "Guardian Phone Number",
         type: "number",
         name: "guardianPhone",
-    },{
+    }, {
         label: "Guardian Email",
         placeHolder: "Guardian Email",
         type: "email",
-         name: "guardianEmail",
-    },{
+        name: "guardianEmail",
+    }, {
         label: "Guardial NID",
         placeHolder: "Guardia NID",
-        type:"number",
-         name: "guardianNID",
-    },{
+        type: "number",
+        name: "guardianNID",
+    }, {
         label: "Address",
         placeHolder: "Address",
         type: "text",
-         name: "address",
-    },{
-        label: "Subject",
-        placeHolder: "Subject",
+        name: "address",
+    }, {
+        label: "Department",
+        placeHolder: "Department",
         type: "text",
-         name: "subject",
-    },{
-        label: "Email",
-        placeHolder: "Email",
-        type: "email",
-         name: "email",
-
-    },{
-        label: "First Name",
-        placeHolder: "First Name"
-    },{
+        name: "department",
+    }, {
         label: "Transcript",
         placeHolder: "Transcript",
         type: "file",
-         name: "transcript",
-    },{
+        name: "transcript",
+    }, {
         label: "Profile Photo",
         placeHolder: "Profile Photo",
         type: "file",
-         name: "url",
+        name: "url",
     }
+
 ]
 
 
