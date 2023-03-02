b811ce6d741d7aac154625b4aae1d9fcb709cde8
Mehedi <mehedihassangd@gmail.com>
2023-03-02 13:06:59 +0600

    add

---
+++ dashboard/src/data/departmentCard.js
@@ -0,0 +1,15 @@
+
+import student from '../imgs/student.png'
+import award from '../imgs/award.png'
+import Department from '../imgs/Department.png'
+import Revenue from '../imgs/Revenue.png'
+
+const departmentCard = [
+    { id: 1, department: 'CSE', student_q: '150', avater: student },
+    { id: 2, department: 'EEE', student_q: '100', avater: award },
+    { id: 1, department: 'Botany', student_q: '120', avater: Department },
+    { id: 1, department: 'Zoology', student_q: '80', avater: Revenue },
+
+];
+
+export default departmentCard;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const routes = [
     { path: '/auth', router: require('./authRoutes.js') },
     { path: '/admission', router: require('./admissionRoutes.js') },
     { path: '/department', router: require('./departmentRoutes.js') },
-    { path: '/payment', router: require('./paymentRoutes.js') }
+    { path: '/payment', router: require('./paymentRoutes.js') },
     {path: '/notice', router: require('./noticeRoutes')}
 ]
 
