bbdabc0437be36238e69c4eb182cc056bf9c8041
Masum-SM <unusmasum0@gmail.com>
2023-03-03 18:04:52 +0600

    contact page update

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./contact.css";
+import emailjs from "emailjs-com";
+import swal from "sweetalert";
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        swal({
+            title: "Good job!",
+            text: "Your Message sent successfullly!!",
+            icon: "success",
+            button: "Aww yiss!",
+          });
+        emailjs
+          .sendForm(
+            "service_2oohr2o",
+            "template_qoqxble",
+            form.current,
+            "user_r7CjpvJ4TlNwjhHFca4tE"
+            
+          )
+          .then(
+            (result) => {
+              console.log(result.text);
+            },
+            (error) => {
+              console.log(error.text);
+            }
+          );
+        e.target.reset();
+      };
     return (
         <div className='mb-32'>
             <div id='Contact_banner'>
@@ -49,10 +78,43 @@ const Contact = () => {
 
             </div>
             <div className='w-4/5 mx-auto '>
-                <div className='my-32 text-center con_formSection'>
-                    <h1 className='text-center'>We would love to hear from you</h1>
+                <div className='mt-32 text-center con_formSection'>
+                    <h1 className='text-center mb-4'>We would love to hear from you</h1>
                     <h3>If you have any question about Admission, Features , faculties , Subject , or any other , feel free to contact us.</h3>
+
                 </div>
+            </div>
+            <div className='w-4/5 mx-auto mt-16'>
+                <form 
+                onSubmit={sendEmail}
+                ref={form}
+                >
+                    <div className='w-4/5 md:w-3/5 lg:w-3/5 mx-auto card bg-base-100 drop-shadow-2xl p-12'>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
+                            <div className="form-control w-full mb-4 con_frm">
+                                <label className="label">Name</label>
+                                <input 
+                                type="text" 
+                                className="input input-bordered w-full" 
+                                
+                                required />
+                            </div>
+
+                            <div className="form-control w-full  mb-4 con_frm">
+                                <label className="label">Email</label>
+                                <input type="email" className="input input-bordered w-full " required/>
+                            </div>
+                        </div>
+
+                        <div className="form-control w-full mb-4 con_frm">
+                            <label className="label">Message</label>
+                            <textarea className="textarea textarea-bordered h-24" required></textarea>
+                        </div>
+                        
+                        <input className='contact_btn mt-4' type="submit" />
+                    </div>
+                    
+                </form>
             </div>
         </div>
 
