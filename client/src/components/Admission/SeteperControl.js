2316054021e3d50242eb55dab978e5b71526039e
Masum-SM <unusmasum0@gmail.com>
2023-03-06 23:55:54 +0600

    update admission button

--- client/src/components/Admission/SeteperControl.js
+++ client/src/components/Admission/SeteperControl.js
@@ -12,7 +12,7 @@ export default function StepperControl({ handleClick, currentStep, steps }) {
 
       <button
         onClick={() => handleClick("next")}
-        className="cursor-pointer rounded-lg bg-green-500 py-2 px-4 font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-slate-700 hover:text-white"
+        className="cursor-pointer rounded-lg bg-indigo-900 py-2 px-4 font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-slate-700 hover:text-white"
       >
         {currentStep === steps.length - 1 ? "Confirm" : "Next"}
       </button>
