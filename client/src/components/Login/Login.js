a313087f728fcb714fa363acd6b547965f5222b8
imranBappy <imranbappy.official@gmail.com>
2023-03-07 19:45:00 +0600

    git error fix

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import "./Login.css";
 import { useForm } from "react-hook-form";
-=========
 import axios from 'axios';
-import { redirect } from "react-router-dom";
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors }, } = useForm();
