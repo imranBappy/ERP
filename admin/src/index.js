a73524d77458e8bd229907f63c064a8d67e28412
imranBappy <imranbappy.official@gmail.com>
2023-03-07 19:44:29 +0600

    done

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
+
 import './index.css';
-=======
+
 import './styles/index.css';
->>>>>>> a84491fd4259b34f074f2b811cb6c7d16ed0738c
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
