f9aadd68013dc44fc39019f70261a355dd65b139
Masum-SM <unusmasum0@gmail.com>
2023-03-10 16:36:20 +0600

    update

--- client/src/components/Shared/Navbar/Navbar.js
+++ client/src/components/Shared/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
         <li className='nav_li'><Link to="/admission" >Admission</Link></li>
         <li className='nav_li'><Link to="/notice" >Notice</Link></li>
         <li className='nav_li'><Link to="/contact" >Contact</Link></li>
-        <li className='nav_li'><Link to="https://dashboard-return-zero.netlify.app" >Dashboard</Link></li>
+
         <li className='nav_li'><Link to="/login" >Login</Link></li>
     </>
 
