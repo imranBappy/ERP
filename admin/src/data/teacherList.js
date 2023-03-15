9d5a2e8952b6cd61f183a317557cb0bd06e0aea0
imranBappy <imranbappy.official@gmail.com>
2023-03-15 14:39:52 +0600

    delete script

--- admin/src/data/teacherList.js
+++
@@ -1,4 +0,0 @@
-const teacherListHead = ["Check", "ID", "Name", "Phone", "Email", "Designation", "Action"];
-const techerDataFields = ["teacherId", "name", "phone", "email", "designation"];
-
-module.exports = teacherListHead;
